f8828cb9a6690a92dcf196b339d130bfd8baf552
Davis <kingdavidcyril102@gmail.com>
2025-12-14 19:33:54 +0100

    Add files via upload

---
+++ setting/config.js
@@ -0,0 +1,92 @@
+const fs = require('fs')
+
+global.owner = "2349125042727" //owner number
+global.footer = "❤🎉𝙴𝙼𝙼𝚈𝙷𝙴𝙽𝚉 𝙼𝙸𝙽𝙸 𝙱𝙾𝚃❤️‍🔥" //footer section
+global.status = false //"self/public" section of the bot
+global.prefa = ['','!','.',',','❤️‍🔥','❤']
+global.owner = ['2349125042727']
+global.xprefix = '.'
+global.gambar = "https://files.catbox.moe/imgbam.jpg"
+global.OWNER_NAME = "@EmmyHenz" //
+global.DEVELOPER = ["7827247982"] //
+global.BOT_NAME = "❤🎉𝙴𝙼𝙼𝚈𝙷𝙴𝙽𝚉 𝙼𝙸𝙽𝙸 𝙱𝙾𝚃❤️‍🔥"
+global.bankowner = "Henshaw Emmanuel"
+global.creatorName = "𝕰𝖒𝖒𝖞𝕳𝖊𝖓𝖟"
+global.ownernumber = '2349125042727'  //creator number
+global.location = "Nigeria, lagos, unknown"
+global.prefa = ['','!','.','#','&']
+//================DO NOT CHANGE OR YOU'LL GET AN ERROR=============\
+global.footer = "𝕰𝖒𝖒𝖞𝕳𝖊𝖓𝖟" //footer section
+global.link = "https://whatsapp.com/channel/0029VangYOt96H4JhFarL10C"
+global.autobio = true//auto update bio
+global.botName = "❤🎉𝙴𝙼𝙼𝚈𝙷𝙴𝙽𝚉 𝙼𝙸𝙽𝙸 𝙱𝙾𝚃❤️‍🔥"
+global.version = "𝙑𝟭"
+global.botname = "❤🎉𝙴𝙼𝙼𝚈𝙷𝙴𝙽𝚉 𝙼𝙸𝙽𝙸 𝙱𝙾𝚃❤️‍🔥"
+global.author = "𝕰𝖒𝖒𝖞𝕳𝖊𝖓𝖟"
+global.themeemoji = '💝'
+global.wagc = 'https://chat.whatsapp.com/F21VFdnoshqIj7JqW8MEIH?mode=ac_t'
+global.thumbnail = 'https://files.catbox.moe/imgbam.jpg'
+global.richpp = ' '
+global.packname = "❤🎉𝙴𝙼𝙼𝚈𝙷𝙴𝙽𝚉 𝙼𝙸𝙽𝙸 𝙱𝙾𝚃❤️‍🔥"
+global.author = "\n\n\n\n\n𝗖𝗿𝗲𝗮𝘁𝗲𝗱 𝗕𝘆 𝕰𝖒𝖒𝖞𝕳𝖊𝖓𝖟\ntelegram : @EmmyHenz"
+global.creator = "2349125042727@s.whatsapp.net"
+global.ownername = '𝕰𝖒𝖒𝖞𝕳𝖊𝖓𝖟' 
+global.onlyowner = `𝗬𝗢𝗨𝗥 𝗡𝗢𝗧 𝗠𝗬 𝗢𝗪𝗡𝗘𝗥❌, Deploy Yours Here Free mini.emmyhenztech.space`
+  // reply 
+global.database = `𝘛𝘰 𝘣𝘦 𝘪𝘯  𝘥𝘢𝘵𝘢𝘣𝘢𝘴𝘦 𝘣𝘢𝘴𝘦 𝘤𝘰𝘯𝘵𝘢𝘤𝘵 𝕰𝖒𝖒𝖞𝕳𝖊𝖓𝖟*`
+  global.mess = {
+wait: "```𝗪𝗔𝗜𝗧......```", 
+   success: "𝑺𝒖𝒄𝒄𝒆𝒔𝒔",
+   on: "❤🎉𝙴𝙼𝙼𝚈𝙷𝙴𝙽𝚉 𝙼𝙸𝙽𝙸 𝙱𝙾𝚃❤️‍🔥 is active", 
+   prem: "FOR PREMIUM USERS ONLY ADD YOUR NUMBER TO DATABASE TO ACCESS PREMIUM", 
+   off: "off",
+   query: {
+       text: "Where's the text?",
+       link: "Where's the link?",
+   },
+   error: {
+       fitur: "Sorry, bro, the feature has error. Please chat with the Bot Developer so it can be fixed immediately.",
+   },
+   only: {
+       group: "Sorry, This Feature Can Only Be Used In Groups only",
+private: "Sorry, This Feature Can Only Be Used In Private Chats",
+       owner: "Sorry, This Feature Can Only Be Used by Queen",
+       admin: " Sorry, this feature can only be used by Bot Admins",
+       badmin: "Sorry, bro, It Looks Like You Can't Use This Feature Because the Bot is Not yet Group Admin",
+       premium: "This feature is specifically for 𝕰𝖒𝖒𝖞𝕳𝖊𝖓𝖟 beloved Premium users",
+   }
+}
+
+global.onlyowner = `\`❤🎉𝙴𝙼𝙼𝚈𝙷𝙴𝙽𝚉 𝙼𝙸𝙽𝙸 𝙱𝙾𝚃❤️‍🔥 \` \n*
+
+    ❌ *Access Denied!* ❌
+𝗦𝗢𝗥𝗥𝗬 𝗬𝗢𝗨𝗥 𝗡𝗢𝗧 𝗠𝗬 𝗢𝗪𝗡𝗘𝗥❌, You Do Not Have Permission To Use This Command. Deploy Yours Here Free mini.emmyhenztech.space`
+  
+global.database = `\`❤🎉𝙴𝙼𝙼𝚈𝙷𝙴𝙽𝚉 𝙼𝙸𝙽𝙸 𝙱𝙾𝚃❤️‍🔥 \` \n*
+
+    ❌ *Access Denied!* ❌
+
+    😢𝗬𝗢𝗨𝗥 𝗡𝗢𝗧 𝗔 𝗣𝗥𝗘𝗠𝗨𝗜𝗠 𝗨𝗦𝗘𝗥😥
+    
+    💚Dm To Purchase Premuim Feature🤩
+
+    *𝗪𝗵𝗮𝘁𝘀𝗮𝗽𝗽: 2349125042727*
+    *𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺: @EmmyHenz*`
+
+global.hituet = 0
+//false=disable and true=enable
+global.autoRecording = true //auto recording
+global.autoTyping = true //auto typing
+global.autorecordtype = true //auto typing + recording
+global.autoread = false //auto read messages
+global.autobio = true //auto update bio
+global.anti92 = true //auto block +92 
+global.autoswview = true //auto view status/story
+
+let file = require.resolve(__filename)
+require('fs').watchFile(file, () => {
+  require('fs').unwatchFile(file)
+  console.log('\x1b[0;32m'+__filename+' \x1b[1;32mupdated!\x1b[0m')
+  delete require.cache[file]
+  require(file)
+})
